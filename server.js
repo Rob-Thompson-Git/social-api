faa654c4cc97145f198f57d05c33f045d7f10b54
RobThompson <rob.thomp15@gmail.com>
2022-10-26 22:20:43 -0500

    added server.js file, completed index.js files for api and routes

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express');
 const db = require('./config/connection');
-// Require model
-const { User, Thought, Reaction } = require('./models');
+const routes = require('./routes');
+
+const cwd = process.cwd();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
+const activity = cwd.includes('01-Activities')
+  ? cwd.split('/01-Activities/')[1]
+  : cwd;
 
-// Finds all users
-// app.get('/users', (req, res) => {
-//   // Using model in route to find all documents that are instances of that model
-//   User.find({}, (err, result) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send(err);
-//     } else {
-//       res.status(200).send(result);
-//     }
-//   });
-// });
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+    console.log(`API server for ${activity} running on port ${PORT}!`);
   });
 });
